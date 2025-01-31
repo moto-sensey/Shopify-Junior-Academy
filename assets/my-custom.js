01cda95cf71e156ee2cec1a484d17cfa046c7c33
SergDev <serg90909@gmail.com>
2025-02-01 00:54:03 +0300

    Add section collection tabs

---
+++ assets/my-custom.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
+   $(document).ready(function () {
+        let toggleTabs = (e) => {
+            e.preventDefault();
+            let parentItem = $(e.currentTarget).parent();
+            let elementActivatePosition = parentItem.index();
+
+            let tabHeadList = parentItem.parent().children();
+            let tabBodyList = parentItem.parent().parent().next().children().children();
+
+            let activateTab = (elements, activeIndex) => {
+                for (let item of elements) {
+                    $(item).removeClass('active');
+                    let elementPosition = $(item).index();
+                    if (activeIndex == elementPosition) {
+                        $(item).addClass('active');
+                    }
+                }
+            }
+            activateTab(tabHeadList, elementActivatePosition);
+            activateTab(tabBodyList, elementActivatePosition);
+        }
+        $(document).on('click', '.list__link', (e) => toggleTabs(e));
+   });
+  });
